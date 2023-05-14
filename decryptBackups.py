2d5cde6eb6be745f48048750d1bb376dd8ca8a8a
Nir Lavi <snirl@c-137.local>
2023-05-14 12:33:36 +0300

    update to zip

--- decryptBackups.py
+++ decryptBackups.py
@@ -1,37 +1,32 @@
 import argparse
 import os
-from cryptography.fernet import Fernet
-
+import zipfile
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Decrypt backups')
-    parser.add_argument('--key', required=True, help='Encryption key')
+    parser.add_argument('--password', required=True, help='Zip password')
     target_group = parser.add_mutually_exclusive_group(required=True)
     target_group.add_argument('--backup-dir', required=False, help='Backup directory')
     target_group.add_argument('--file', required=False, help='File to decrypt')
     return parser.parse_args()
 
 
-def decrypt_file(file_path, enc_key):
+def decrypt_file(file_path, password):
     print(f"\nDecrypting:\n\t{file_path}")
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    fernet = Fernet(enc_key)
-    decrypted = fernet.decrypt(data)
-    dec_path = file_path.replace('.enc', '')
-    with open(dec_path, 'wb') as f:
-        f.write(decrypted)
-    print(f"Decrypted file saved to: {dec_path}")
+    with zipfile.ZipFile(file_path, "r") as zip_file:
+        zip_file.setpassword(password.encode("utf-8"))
+        zip_file.extractall()
 
 
 def main():
     args = parse_args()
-    enc_key = args.key.encode()
+    password = args.password
     if args.backup_dir:
         if not os.path.isdir(args.backup_dir):
             print(f"Error: {args.backup_dir} is not a valid directory.")
             return
-        files = [os.path.join(args.backup_dir, f) for f in os.listdir(args.backup_dir)]
+        files = [os.path.join(args.backup_dir, f) for f in os.listdir(args.backup_dir) if os.path.splitext(f)[1] == ".zip"]
+
     else:
         if not os.path.isfile(args.file):
             print(f"Error: {args.file} is not a valid file.")
@@ -39,7 +34,8 @@ def main():
         files = [args.file]
 
     for file in files:
-        decrypt_file(file, enc_key)
+        print(f'Decrypting {file}')
+        decrypt_file(file, password=password)
 
 
 if __name__ == "__main__":
--- uploadBackups.py
+++ uploadBackups.py
@@ -1,14 +1,14 @@
 import os
 import sys
 import argparse
+import zipfile
 from collections import defaultdict
-from cryptography.fernet import Fernet
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Upload backups to Azure Blob Storage')
-    parser.add_argument('--key', required=True, help='Encryption key')
+    parser.add_argument('--password', required=True, help='Zip password')
     parser.add_argument('--backup-dir', required=True, help='Backup directory')
     parser.add_argument('--connection-string', required=True, help='Azure Blob Storage connection string')
     return parser.parse_args()
@@ -17,7 +17,7 @@ def parse_args():
 def main():
     args = parse_args()
 
-    enc_key = args.key.encode()
+    password = args.password
     backup_dir = args.backup_dir
     connection_str = args.connection_string
 
@@ -34,7 +34,7 @@ def main():
         container_client.create_container()
 
     files_to_upload = enumerate_backups_to_upload(backup_dir, backup_ext)
-    upload_files(container_client, backup_dir, enc_key, files_to_upload)
+    upload_files(container_client, backup_dir, password, files_to_upload)
 
 
 def enumerate_backups_to_upload(backup_dir, backup_ext):
@@ -55,27 +55,24 @@ def enumerate_backups_to_upload(backup_dir, backup_ext):
     return latest_files.values()
 
 
-def encrypt_file(file_path, enc_key, host):
+def zip_and_encrypt_file(file_path, password, host):
     # Encrypt the file
-    print(f"\nEncrypting:\n\t{file_path}")
-    with open(file_path, mode="rb") as data:
-        file_data = data.read()
-        f = Fernet(enc_key)
-        encrypted_data = f.encrypt(file_data)
-        enc_path = f"{file_path}.{host}.enc"
-        with open(enc_path, mode="wb") as data:
-            data.write(encrypted_data)
+    print(f"\nZipping and Encrypting:\n\t{file_path}")
+    zip_file_path = f"{file_path}.{host}.zip"
+    with zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_LZMA) as zip_file:
+        zip_file.setpassword(password.encode("utf-8"))
+        zip_file.write(file_path)
         print("Done")
-    return enc_path
+    return zip_file_path
 
 
-def upload_files(container_client, backup_dir, enc_key, files_to_upload):
+def upload_files(container_client, backup_dir, password, files_to_upload):
     try:
         for file in files_to_upload:
             file_path = os.path.join(backup_dir, file)
             with open(f"{file_path}.notes", mode="r") as data:
                 host = data.read()
-            enc_path = encrypt_file(file_path, enc_key, host)
+            enc_path = zip_and_encrypt_file(file_path, password, host)
             blob_name = os.path.basename(enc_path).replace('_', '-')
             blob_client = container_client.get_blob_client(blob=blob_name)
             if blob_client.exists() == False:
